f8bec693d6e840bcb3d49e356500760b0ee74b6e
Zaperox <admin@carolineleeceramics.co.uk>
2017-09-15 16:45:29 +0100

    Initial commit

---
+++ DiscordWoT/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using Discord.Net.Providers.WS4Net;
+using Newtonsoft.Json;
+using System.IO;
+using System.Linq;
+
+namespace DiscordWoT
+{
+    class Program
+    {
+        public static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+
+        public async Task MainAsync()
+        {
+            Directory.CreateDirectory("Users");
+            _client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                WebSocketProvider = WS4NetProvider.Instance
+            });
+
+            _client.Log += Log;
+            _client.MessageReceived += MessageReceived;
+
+            string token = File.ReadLines("Token.txt").First(); // Remember to keep this private!
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+
+        private async Task MessageReceived(SocketMessage message)
+        {
+            if (message.Content.ToLower() == "!ping")
+            {
+                await message.DeleteAsync();
+                await message.Channel.SendMessageAsync("Pong!");
+            }
+            else if (message.Content.ToLower().StartsWith("!wotadd "))
+            {
+                string WoTName = message.Content.ToLower().Replace("!wotadd ", "");
+                WoTUser UserObj = new WoTUser(message.Author, WoTName);
+                Console.WriteLine("Adding a new user.");
+                using (StreamWriter writer = File.CreateText("Users/" + UserObj.Id.ToString()))
+                {
+                    await writer.WriteAsync(JsonConvert.SerializeObject(UserObj, Formatting.Indented));
+                }
+                Console.WriteLine("Done adding a new user.");
+                await message.DeleteAsync();
+                await message.Channel.SendMessageAsync("Done!");
+            }
+        }
+
+        private Task SaveUserData()
+        {
+
+            return Task.CompletedTask;
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ DiscordWoT/WoTUser.cs
@@ -0,0 +1,20 @@
+﻿using Discord.WebSocket;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace DiscordWoT
+{
+    class WoTUser
+    {
+        public ulong Id;
+        public string nickname;
+        public string WoTName;
+
+        public WoTUser(SocketUser DiscordUser, string GivenWoTName)
+        {
+            Id = DiscordUser.Id;
+            nickname = DiscordUser.Username;
+            WoTName = GivenWoTName;
+        }
+    }
+}
